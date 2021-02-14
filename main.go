3ad7db9d352ead25a31ee02f4ac96ce794dc423b
aag2807 <aagn2807@gmail.com>
2021-02-14 16:01:27 -0400

    api

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+//Roll is the basic outline for the sushi
+type Roll struct {
+	ID          string `json:"id"`
+	ImageNumber string `json:"imageNumber"`
+	Name        string `json:"name"`
+	Ingredients string `json:"ingredients"`
+}
+
+//Rools as a slice
+var rolls []Roll
+
+//index
+func getRolls(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(rolls)
+}
+
+//Show
+func getRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range rolls {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+
+}
+
+//Create
+func createRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newRoll Roll
+	json.NewDecoder(r.Body).Decode(&newRoll)
+	newRoll.ID = strconv.Itoa(len(rolls) + 1)
+	rolls = append(rolls, newRoll)
+	json.NewEncoder(w).Encode((newRoll))
+}
+
+//Update
+func updateRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, item := range rolls {
+		if item.ID == params["id"] {
+			rolls = append(rolls[:i], rolls[i+1:]...)
+			var newRoll Roll
+			json.NewDecoder(r.Body).Decode(&newRoll)
+			newRoll.ID = params["id"]
+			rolls = append(rolls, newRoll)
+			json.NewEncoder(w).Encode(newRoll)
+			return
+		}
+	}
+
+}
+
+//Delete
+func deleteRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, item := range rolls {
+		if item.ID == params["id"] {
+			rolls = append(rolls[:i], rolls[i+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(rolls)
+}
+
+func main() {
+
+	//mock sushi
+	rolls = append(rolls,
+		Roll{ID: "1", ImageNumber: "8", Name: "Spicy Tuna Roll", Ingredients: "Tuna, Cream Cheese, Rice, Avocado, Eel sauce"},
+		Roll{ID: "2", ImageNumber: "6", Name: "Komodo Roll", Ingredients: "Crispy Tempura, Crab, Shrimp , Salmon, Cream Cheese, The secret House Sauce"},
+	)
+
+	//Initializing Router
+	router := mux.NewRouter()
+
+	// declaring the endpoints
+	router.HandleFunc("/sushi", getRolls).Methods("GET")
+	router.HandleFunc("/sushi/{id}", getRoll).Methods("GET")
+	router.HandleFunc("/sushi", createRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", updateRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", deleteRoll).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
